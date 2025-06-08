bd11e65314cf0ee5952c0bf1dd5a5f8c9a279024
FukKung <118256125+FukKung@users.noreply.github.com>
2025-06-08 22:56:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3, // แสดงการ์ด 3 ใบพร้อมกัน
-  spaceBetween: 30, // ระยะห่างระหว่างการ์ด
+  slidesPerView: 3,
+  spaceBetween: 30,
   centeredSlides: true,
   loop: true,
   slideToClickedSlide: true,
-  effect: 'coverflow', // เพิ่มเอฟเฟกต์การหมุน
+  effect: 'coverflow', 
   coverflowEffect: {
     rotate: 30,
     stretch: 0,
@@ -16,9 +16,9 @@ const swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
   on: {
-    slideChangeTransitionEnd: () => { // ใช้ slideChangeTransitionEnd แทน slideChange
-      const audio = new Audio('mac-quack.mp3'); // ระบุไฟล์เสียงที่ต้องการ
-      audio.play(); // เล่นเสียงเมื่อเลื่อนรูปเสร็จสมบูรณ์
+    slideChangeTransitionEnd: () => {
+      const audio = new Audio('mac-quack.mp3');
+      audio.play(); 
     }
   }
 });
